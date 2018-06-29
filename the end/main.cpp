bfd3c5c02048496fe7d89fe8a1e47d8368f577b7
20171105134 <s20171105134@admindeiMac-32.local>
2018-06-29 09:57:16 +0800

    don't have any mind
    uncomplete,

--- the end/main.cpp
+++ the end/main.cpp
@@ -11,25 +11,50 @@
 #include<cstring>
 #include<fstream>
 #include<cstdlib>
+#include <streambuf>
+#include <sstream>
+#include <vector>
 using namespace std;
-int main()
+string Trim(string& str)
 {
-    string  name;
-    int number;
-    string sex;
-    string classes;
-    int tel;
-    int a,b,c,d,e;
-    int max=0;
-    for (int i = 0; i < 5; i++)
-    {
-        if (max < a[i])
-            max = a[i]
-    }
-    int min=100;
-    for (int j=0;i<5;j++)
-    {
-        if(min>a[i])
-            min=a[i]
-    }
+    //str.find_first_not_of(" \t\r\n"),在字符串str中从索引0开始，返回首次不匹配"\t\r\n"的位置
+    str.erase(0,str.find_first_not_of(" \t\r\n"));
+    str.erase(str.find_last_not_of(" \t\r\n") + 1);
+    return str;
 }
+int main()
+{ifstream inFile("studentdata2.csv", ios::in);
+    string lineStr;
+    vector<vector<string>> strArray;
+    int array[3][3];
+    int i,j;
+    i=0;
+    char* end;
+    if(inFile.fail())
+        cout<<"读取文件失败"<<endl;
+    while (getline(inFile, lineStr))
+    {
+        j=0;
+        // 打印整行字符串
+        cout << lineStr << endl;
+        // 存成二维表结构
+        stringstream ss(lineStr);
+        string str;
+        vector<string> lineArray;
+        // 按照逗号分隔
+        while (getline(ss, str, ','))
+        {
+            array[i][j]=static_cast<int>(strtol(str.c_str(),&end,10));              //string -> int
+            j++;
+        }
+        i++;
+}
+for(int i=0;i<3;i++)
+{
+    for(int j=0;j<3;j++)
+        cout<<array[i][j];
+        cout<<endl;
+        }
+        getchar();
+        return 0;
+        }
