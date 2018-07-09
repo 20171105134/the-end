babd1048253b1ae000ac2a06ded7ff142a9d4404
20171105134 <15049122091@163.com>
2018-07-09 14:11:38 +0800

    chage make me crazy

--- the end/main.cpp
+++ the end/main.cpp
@@ -18,6 +18,8 @@ struct c
     int tel[20];
     int point[20];
     double sum[20];
+    double max,min;
+    
 };
 
 string Trim(string& str)
@@ -33,18 +35,10 @@ int main( int argc,const char * argv[])
     struct c a[2000];
     FILE *s;
     FILE*b;
-    char name[20];
-    int number[20];
-    char sex[5];
-    char classes[20];
-    int score[20];
-    int rater1[20],rater2[20],rater3[20],rater4[20],rater5[20];
-    int tel[20];
-    int point[20];
-    int sum[20];
-    int brith;
+    double sum[20];
+    
     int i=0,j=0,n=0;
-    int b;
+    
     ifstream fin("/Users/s20171105134/Desktop/the end/the end/data.csv");
     string line;
     while (getline(fin,line))
@@ -57,31 +51,35 @@ int main( int argc,const char * argv[])
     }
     else
     {
-        while(fscanf(s,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ",a[i].number,a[i].name,a[i].sex,a[i].brith,a[i].classes,a[i]tel,a[i].rater1,a[i].rater2,a[i].rater3,a[i].rater4,a[i].rater5))
+        while(fscanf(s,"%d ,%s ,%s ,%d ,%s ,%d ,%d ,%d ,%d ,%d ,%d",a[i].number,a[i].name,a[i].sex,a[i].brith,a[i].classes,a[i]tel,a[i].rater1,a[i].rater2,a[i].rater3,a[i].rater4,a[i].rater5))
+      
         {
             s[i].min=rater[1];
-            for(i=0;j<5;i++)
+            for(i=0;j<5;j++)
             {
-                if (a[i].point<s[i].min)
+                
+                if (a[i].rater[j]<s[i].min)
                   {
-                      s[i].min=s[i].score[j];
+                      s[i].min=s[i].rater[j];
                   }
             }
             a[i].max=rater[1];
-            for(i=0;i<5;i++)
+            for(i=0;j<5;j++)
             {
-               if(a[i].rater>a[i].max)
+               if(a[i].rater[j]>a[i].max)
                {
                    a[i].max=a[i].rater;
                }
             }
         }
+    ｝
         
              /* for(i=0;i<j;i++)
         {
             printf("%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d\n",a[i].number,a[i].name,a[i].sex,a[i].brith,a[i].classes,a[i].tel,a[i].rater1,a[i].rater2,a[i].rater3,a[i].rater4,a[i].rater5);
         }*/
-    for(i=0;i<b;i++)
+    a[i].min＝a[i].point[0];
+    for(i=0;j<5;j++)
     {
         
         a[i].sum=(a[i].rater1+a[i].rater2+a[i].rater3+a[i].rater4+a[i].rater5);
