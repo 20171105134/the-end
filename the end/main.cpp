074c34234334fc6de7cb2507f3879e1aed606e09
20171105134 <15049122091@163.com>
2018-07-09 15:48:16 +0800

    change a lot

--- the end/main.cpp
+++ the end/main.cpp
@@ -9,86 +9,109 @@ using namespace std;
 struct c
 {
     char name[20];
-    int number[20];
+    int number;
     int brith;
-    char sex[5];
+    char sex[10];
     char classes[20];
     int score[20];
-    int rater1[20],rater2[20],rater3[20],rater4[20],rater5[20];
-    int tel[20];
-    int point[20];
-    double sum[20];
-    double max,min;
+    int rater[20];
+    char tel[20];
+    int point;
+    int sum=0;
+    int max,min;
     
 };
 
-string Trim(string& str)
-{
-    //str.find_first_not_of(" \t\r\n"),在字符串str中从索引0开始，返回首次不匹配"\t\r\n"的位置
-    str.erase(0,str.find_first_not_of(" \t\r\n"));
-    str.erase(str.find_last_not_of(" \t\r\n") + 1);
-    return str;
-}
-
-int main( int argc,const char * argv[])
+int main()
 {
     struct c a[2000];
     FILE *s;
-    FILE*b;
-    double sum[20];
+   // FILE*b;
+  //  double sum[20];
     
-    int i=0,j=0,n=0;
+    char bt[100];
+    int i=0,j=0,n=0,e;
+   // int i=0;
+    //printf("1");
     
-    ifstream fin("/Users/s20171105134/Desktop/the end/the end/data.csv");
+       printf("%s",bt);
+   /* ifstream fin("/Users/s20171105134/Desktop/data.csv");
     string line;
     while (getline(fin,line))
     {
         n++;
-    }
-    if((s=fopen("/Users/s20171105134/Desktop/the end/the end/data.csv","r"))==0)
+    }*/
+   if((s=fopen("/Users/s20171105134/Desktop/student1.csv","r"))==NULL)
     {
         printf("文件不存在\n");
+        exit(-1);
     }
     else
     {
-        while(fscanf(s,"%d ,%s ,%s ,%d ,%s ,%d ,%d ,%d ,%d ,%d ,%d",a[i].number,a[i].name,a[i].sex,a[i].brith,a[i].classes,a[i]tel,a[i].rater1,a[i].rater2,a[i].rater3,a[i].rater4,a[i].rater5))
+        fscanf(s,"%s",bt);       // fscanf(s,"%*[^\n]%*c");
+        printf("1");
       
+        while(!feof(s))
         {
-            s[i].min=rater[1];
-            for(i=0;j<5;j++)
+            fscanf(s,"%d,%[^,],%[^,],%d,%[^,],%[^,]",&a[i].number,a[i].name,a[i].sex,&a[i].brith,a[i].classes,a[i].tel);
+           //
+            for(e=0;e<5;e++)
             {
-                
-                if (a[i].rater[j]<s[i].min)
-                  {
-                      s[i].min=s[i].rater[j];
-                  }
+                fscanf(s,"%d",&a[i].rater[e]);
             }
-            a[i].max=rater[1];
-            for(i=0;j<5;j++)
-            {
-               if(a[i].rater[j]>a[i].max)
-               {
-                   a[i].max=a[i].rater;
-               }
-            }
+            i++;
+            //printf("%d",i);
         }
-    ｝
-        
+        printf("%s",bt);
+        fclose(s);
+    }
+   // printf("%d",a[0].rater[0]);
+    j=i;
+    for(i=0;i<j;i++)
+    {
+        a[i].max=a[i].rater[0];
+        a[i].min=a[i].rater[0];
+    }
+    j=i;
+ 
+            for(i=0;i<j;j++)
+            {
+                for(e=0;e<5;e++)
+                 {
+  
+                if (a[i].rater[e]<a[i].min)
+                  {
+                      a[i].min=a[i].rater[e];
+                  }
+                if(a[i].rater[e]>a[i].max)
+                   {
+                       a[i].max=a[i].rater[e];
+                   }
+
+                 }
+            }
+    
              /* for(i=0;i<j;i++)
         {
             printf("%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d\n",a[i].number,a[i].name,a[i].sex,a[i].brith,a[i].classes,a[i].tel,a[i].rater1,a[i].rater2,a[i].rater3,a[i].rater4,a[i].rater5);
         }*/
-    a[i].min＝a[i].point[0];
-    for(i=0;j<5;j++)
+                     
+                     
+   
+    j=i;
+    for(i=0;i<j;i++)
     {
-        
-        a[i].sum=(a[i].rater1+a[i].rater2+a[i].rater3+a[i].rater4+a[i].rater5);
+        for(e=0;e<5;e++)
+    {
+        a[i].sum=a[i].rater[e]+a[i].sum;
+        a[i].point =(a[i].sum-a[i].max-a[i].min)/3;
 
     }
-    j=i;    for(i=0;i<j;i++)
-    {
-        a[i].point=(a[i].sum-max[i]-min[i])/3;
     }
-        //fprintf其作用是格式化输出到一个流/文件中
+    
+        
+        
+            //fprintf其作用是格式化输出到一个流/文件中
         return 0;
 }
+
