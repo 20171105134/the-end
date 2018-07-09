c9687b35fff2cd86344b3feebdddf4d0399b5f22
20171105134 <15049122091@163.com>
2018-07-09 15:59:11 +0800

    need to do more

--- the end/main.cpp
+++ the end/main.cpp
@@ -1,3 +1,4 @@
+ 
 #include <iostream>
 #include<string>
 #include<cstring>
@@ -26,22 +27,22 @@ int main()
 {
     struct c a[2000];
     FILE *s;
-   // FILE*b;
-  //  double sum[20];
+    FILE*b;
+    //  double sum[20];
     
     char bt[100];
     int i=0,j=0,n=0,e;
-   // int i=0;
+    // int i=0;
     //printf("1");
     
-       printf("%s",bt);
-   /* ifstream fin("/Users/s20171105134/Desktop/data.csv");
-    string line;
-    while (getline(fin,line))
-    {
-        n++;
-    }*/
-   if((s=fopen("/Users/s20171105134/Desktop/student1.csv","r"))==NULL)
+    printf("%s",bt);
+    /* ifstream fin("/Users/s20171105134/Desktop/data.csv");
+     string line;
+     while (getline(fin,line))
+     {
+     n++;
+     }*/
+    if((s=fopen("/Users/s20171105134/Desktop/student1.csv","r"))==NULL)
     {
         printf("文件不存在\n");
         exit(-1);
@@ -50,11 +51,11 @@ int main()
     {
         fscanf(s,"%s",bt);       // fscanf(s,"%*[^\n]%*c");
         printf("1");
-      
+        
         while(!feof(s))
         {
             fscanf(s,"%d,%[^,],%[^,],%d,%[^,],%[^,]",&a[i].number,a[i].name,a[i].sex,&a[i].brith,a[i].classes,a[i].tel);
-           //
+            //
             for(e=0;e<5;e++)
             {
                 fscanf(s,"%d",&a[i].rater[e]);
@@ -65,7 +66,7 @@ int main()
         printf("%s",bt);
         fclose(s);
     }
-   // printf("%d",a[0].rater[0]);
+    // printf("%d",a[0].rater[0]);
     j=i;
     for(i=0;i<j;i++)
     {
@@ -73,45 +74,59 @@ int main()
         a[i].min=a[i].rater[0];
     }
     j=i;
- 
-            for(i=0;i<j;j++)
-            {
-                for(e=0;e<5;e++)
-                 {
-  
-                if (a[i].rater[e]<a[i].min)
-                  {
-                      a[i].min=a[i].rater[e];
-                  }
-                if(a[i].rater[e]>a[i].max)
-                   {
-                       a[i].max=a[i].rater[e];
-                   }
-
-                 }
-            }
     
-             /* for(i=0;i<j;i++)
+    for(i=0;i<j;j++)
+    {
+        for(e=0;e<5;e++)
         {
-            printf("%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d\n",a[i].number,a[i].name,a[i].sex,a[i].brith,a[i].classes,a[i].tel,a[i].rater1,a[i].rater2,a[i].rater3,a[i].rater4,a[i].rater5);
-        }*/
-                     
-                     
-   
+            
+            if (a[i].rater[e]<a[i].min)
+            {
+                a[i].min=a[i].rater[e];
+            }
+            if(a[i].rater[e]>a[i].max)
+            {
+                a[i].max=a[i].rater[e];
+            }
+            
+        }
+    }
+    
+    /* for(i=0;i<j;i++)
+     {
+     printf("%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d\n",a[i].number,a[i].name,a[i].sex,a[i].brith,a[i].classes,a[i].tel,a[i].rater1,a[i].rater2,a[i].rater3,a[i].rater4,a[i].rater5);
+     }*/
+    
+    
+    
     j=i;
     for(i=0;i<j;i++)
     {
         for(e=0;e<5;e++)
+        {
+            a[i].sum=a[i].rater[e]+a[i].sum;
+            a[i].point =(a[i].sum-a[i].max-a[i].min)/3;
+            
+        }
+    }
+    j=i;
+    b=fopen("/Usres/s20171105134/Desktop/student2.csv","w");
+    fprintf(b,"%s",bt);
+    i=0;
+    while(i<j)
     {
-        a[i].sum=a[i].rater[e]+a[i].sum;
-        a[i].point =(a[i].sum-a[i].max-a[i].min)/3;
-
+        fprintf(b,"%d,%s,%s,%d,%s,%s",a[i].number,a[i].name,a[i].sex,a[i].brith,a[i].classes,a[i].tel);
+        for(e=0;e<5;e++)
+        {
+            fprintf(b,"%d",a[i].rater[e]);
+        }
+        fprintf(b,"%d",a[i].point);
+        fprintf(b,"\n");
+        i++;
     }
-    }
+    fclose(b);
     
-        
-        
-            //fprintf其作用是格式化输出到一个流/文件中
-        return 0;
+    //fprintf其作用是格式化输出到一个流/文件中
+    return 0;
 }
 
